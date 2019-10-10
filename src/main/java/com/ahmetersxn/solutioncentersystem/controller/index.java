7eac4e5b83ef3ec6f66b59f90518339895bf9184
ahmet ersin <ahmetersxn@gmail.com>
2019-10-10 17:22:19 +0300

    INDEX FIXED

---
+++ src/main/java/com/ahmetersxn/solutioncentersystem/controller/index.java
@@ -0,0 +1,23 @@
+package com.ahmetersxn.solutioncentersystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class index {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String welcome() {
+        return "master";
+    }
+
+    @RequestMapping(value={"/pcs","/index.html"})
+    public ModelAndView index() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("index");
+        return mav;
+    }
+}
